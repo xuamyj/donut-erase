86f922c74e0d896045cf86b6527c60451a182427
Amy Xu <xuamyj@stanford.edu>
2015-12-30 09:18:55 -0500

    hooked up to localStorage

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var wordCount = document.getElementById('wordcount');
-var status = document.getElementById('status');
+var msg = document.getElementById('msg');
+var textBox = document.getElementById('words');
 var commitButton = document.getElementById('commit');
 var finishButton = document.getElementById('finish');
 
-function updateStatus(statusMessage) {
-  status.text(statusMessage);
+if(!localStorage.getItem('everythingSoFar')) {
+  localStorage.setItem('everythingSoFar', ' ');
 }
 
-commitButton.onclick = updateStatus; // "commit"
-finishButton.onclick = updateStatus; // "finish"
+function commit() {
+  msg.innerHTML = "commit";
+  var everythingSoFar = localStorage.getItem('everythingSoFar') + ' ' + textBox.value
+  localStorage.setItem('everythingSoFar', everythingSoFar);
+  textBox.value = '';
+  return false;
+}
+
+function finish() {
+  msg.innerHTML = localStorage.getItem('everythingSoFar');
+  localStorage.removeItem('everythingSoFar');
+  return false;
+}
+
+commitButton.onclick = commit; // "commit"
+finishButton.onclick = finish; // "finish"
